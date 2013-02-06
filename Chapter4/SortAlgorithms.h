0e902f6792c2623a01cffee92093d43f99689d59
Mike Sharpe <masrtis@gmail.com>
2013-02-05 17:09:05 -0800

    Fix strict weak ordering assert generated by make_heap debug code in STL.
    Implement function adapter BinarySwap that swaps the arguments passed to
    a binary function. Implement helper function makeSwap that constructs a
    BinarySwap adapter. Use makeSwap in priority_queue declaration.

--- Chapter4/SortAlgorithms.h
+++ Chapter4/SortAlgorithms.h
@@ -4,6 +4,33 @@
 #include <functional>
 #include <iterator>
 
+template <typename BinFn>
+class BinarySwap : public std::binary_function<typename BinFn::second_argument_type,
+                                               typename BinFn::first_argument_type,
+                                               bool>
+{
+public:
+    explicit BinarySwap(BinFn fn)
+        : m_fn(fn)
+    {
+
+    }
+
+    bool operator()(const typename BinFn::second_argument_type& lhs,
+                    const typename BinFn::first_argument_type& rhs) const
+    {
+        return m_fn(rhs, lhs);
+    }
+private:
+    BinFn m_fn;
+};
+
+template <typename BinFn>
+BinarySwap<BinFn> makeSwap(BinFn fn)
+{
+    return BinarySwap<BinFn>(fn);
+}
+
 template <typename SortAlgorithm>
 class Sorter
 {
@@ -33,8 +60,7 @@ struct HeapSortAlgorithm
     void operator()(It begin, It end, Comp compFunc) const
     {
         typedef typename It::value_type value_type;
-        std::priority_queue<value_type, std::vector<value_type>, decltype(std::not2(compFunc))> heap(begin, end, std::not2(compFunc));
-
+        std::priority_queue<value_type, std::vector<value_type>, decltype(makeSwap(compFunc))> heap(begin, end, makeSwap(compFunc));
         std::for_each(begin, end, [&](value_type& elem) { elem = heap.top(); heap.pop(); });
     }
 };
