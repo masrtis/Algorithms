98370c15a725cbd3e9e31a8e98d501f259bc3f2c
Michael Sharpe <masrtis@gmail.com>
2013-03-03 16:49:10 -0800

    Refactor test cases.
    Add separate test case for EmptyQuicksort. Refactor test cases into a
    template function to reduce amount of code. Make sure test data is copied,
    not re-shuffled. This ensures test data is the same for each algorithm,
    improving the comparison of running time.

--- Chapter4/main.cpp
+++ Chapter4/main.cpp
@@ -16,15 +16,14 @@ std::string generateRandomString(RandFunc& rng, std::string::size_type size)
     return result;
 }
 
-BOOST_AUTO_TEST_CASE(SortRandomStrings)
+template <typename SortType, typename GenerateFunc>
+void runTestCase(size_t n, GenerateFunc generator, bool runNSquaredTests)
 {
-    std::vector<std::string> testData;
-    const size_t n = 1000000;
-    testData.reserve(n);
+    std::vector<SortType> randomValues;
+    randomValues.reserve(n);
+    std::generate_n(std::back_inserter(randomValues), n, generator);
 
-    auto charGenerator(std::bind(std::uniform_int_distribution<>(1, 255), std::mt19937()));
-    auto strSizeGenerator(std::bind(std::uniform_int_distribution<>(128, 1024), std::mt19937()));
-    std::generate_n(std::back_inserter(testData), n, [&](){ return generateRandomString(charGenerator, strSizeGenerator()); });
+    std::vector<SortType> testData(begin(randomValues), end(randomValues));
 
     {
         boost::timer::auto_cpu_timer t(3);
@@ -32,24 +31,8 @@ BOOST_AUTO_TEST_CASE(SortRandomStrings)
         std::cout << "Standard sort elapsed CPU time:";
     }
 
-    std::mt19937 rng;
-    std::shuffle(begin(testData), end(testData), rng);
+    testData = randomValues;
 
-    //{
-    //    boost::timer::auto_cpu_timer t(3);
-    //    selectionSort(begin(testData), end(testData));
-    //    std::cout << "Selection sort elapsed CPU time:";
-    //}
-    //BOOST_CHECK(std::is_sorted(begin(testData), end(testData)));
-
-    //{
-    //    boost::timer::auto_cpu_timer t(3);
-    //    selectionSort(begin(testData), end(testData), std::greater<std::string>());
-    //    std::cout << "Selection sort (>) elapsed CPU time:";
-    //}
-    //BOOST_CHECK(std::is_sorted(begin(testData), end(testData), std::greater<std::string>()));
-
-    std::list<std::string> testList(begin(testData), end(testData));
     {
         boost::timer::auto_cpu_timer t(3);
         heapSort(begin(testData), end(testData));
@@ -57,6 +40,7 @@ BOOST_AUTO_TEST_CASE(SortRandomStrings)
     }
     BOOST_CHECK(std::is_sorted(begin(testData), end(testData)));
 
+    std::list<SortType> testList(begin(randomValues), end(randomValues));
     {
         boost::timer::auto_cpu_timer t(3);
         heapSort(begin(testList), end(testList));
@@ -64,16 +48,7 @@ BOOST_AUTO_TEST_CASE(SortRandomStrings)
     }
     BOOST_CHECK(std::is_sorted(begin(testList), end(testList)));
 
-    std::shuffle(begin(testData), end(testData), rng);
-
-    //{
-    //    boost::timer::auto_cpu_timer t(3);
-    //    insertionSort(begin(testData), end(testData));
-    //    std::cout << "Insertion sort elapsed CPU time:";
-    //}
-    //BOOST_CHECK(std::is_sorted(begin(testData), end(testData)));
-
-    std::shuffle(begin(testData), end(testData), rng);
+    testData = randomValues;
 
     {
         boost::timer::auto_cpu_timer t(3);
@@ -81,78 +56,60 @@ BOOST_AUTO_TEST_CASE(SortRandomStrings)
         std::cout << "Quick sort elapsed CPU time:";
     }
     BOOST_CHECK(std::is_sorted(begin(testData), end(testData)));
+
+    if (runNSquaredTests)
+    {   
+        testData = randomValues;
+        
+        {
+            boost::timer::auto_cpu_timer t(3);
+            selectionSort(begin(testData), end(testData));
+            std::cout << "Selection sort elapsed CPU time:";
+        }
+        BOOST_CHECK(std::is_sorted(begin(testData), end(testData)));
+
+        testData = randomValues;
+
+        {
+            boost::timer::auto_cpu_timer t(3);
+            selectionSort(begin(testData), end(testData), std::greater<SortType>());
+            std::cout << "Selection sort (>) elapsed CPU time:";
+        }
+        BOOST_CHECK(std::is_sorted(begin(testData), end(testData), std::greater<SortType>()));
+
+        testData = randomValues;
+
+        {
+            boost::timer::auto_cpu_timer t(3);
+            insertionSort(begin(testData), end(testData));
+            std::cout << "Insertion sort elapsed CPU time:";
+        }
+        BOOST_CHECK(std::is_sorted(begin(testData), end(testData)));
+
+        testData = randomValues;
+    }
+}
+
+BOOST_AUTO_TEST_CASE(SortRandomStrings)
+{
+    auto charGenerator(std::bind(std::uniform_int_distribution<>(1, 255), std::mt19937()));
+    auto strSizeGenerator(std::bind(std::uniform_int_distribution<>(128, 512), std::mt19937()));
+    
+    std::cout << "Sort random strings:" << std::endl;
+    runTestCase<std::string>(100000, [&]{ return generateRandomString(charGenerator, strSizeGenerator()); }, true);
+    std::cout << std::endl;
 }
 
 BOOST_AUTO_TEST_CASE(SortRandomDieRolls)
 {
-    std::vector<std::mt19937::result_type> testData;
-    const size_t n = 100000;
-    testData.reserve(n);
-
     auto dieRoll = std::bind(std::uniform_int_distribution<>(1, 32), std::discard_block_engine<std::mt19937, 256, 32>());
-    std::generate_n(std::back_inserter(testData), n, dieRoll);
-
-    {
-        boost::timer::auto_cpu_timer t(3);
-        std::sort(begin(testData), end(testData));
-        std::cout << "Standard sort elapsed CPU time:";
-    }
     
-    std::mt19937 rng;
-    std::shuffle(begin(testData), end(testData), rng);
+    std::cout << "Sort random die rolls:" << std::endl;
+    runTestCase<std::mt19937::result_type>(100000, dieRoll, true);
+}
 
-    {
-        boost::timer::auto_cpu_timer t(3);
-        selectionSort(begin(testData), end(testData));
-        std::cout << "Selection sort elapsed CPU time:";
-    }
-    BOOST_CHECK(std::is_sorted(begin(testData), end(testData)));
-
-    std::shuffle(begin(testData), end(testData), rng);
-
-    {
-        boost::timer::auto_cpu_timer t(3);
-        selectionSort(begin(testData), end(testData), std::greater<std::mt19937::result_type>());
-        std::cout << "Selection sort (>) elapsed CPU time:";
-    }
-    BOOST_CHECK(std::is_sorted(begin(testData), end(testData), std::greater<std::mt19937::result_type>()));
-
-    {
-        boost::timer::auto_cpu_timer t(3);
-        heapSort(begin(testData), end(testData));
-        std::cout << "Heap sort elapsed CPU time:";
-    }
-    BOOST_CHECK(std::is_sorted(begin(testData), end(testData)));
-
-    std::shuffle(begin(testData), end(testData), rng);
-
-    std::list<std::mt19937::result_type> testList(begin(testData), end(testData));
-    {
-
-        boost::timer::auto_cpu_timer t(3);
-        heapSort(begin(testList), end(testList));
-        std::cout << "Heap sort (list) elapsed CPU time:";
-    }
-    BOOST_CHECK(std::is_sorted(begin(testList), end(testList)));
-
-    std::shuffle(begin(testData), end(testData), rng);
-
-    {
-        boost::timer::auto_cpu_timer t(3);
-        insertionSort(begin(testData), end(testData));
-        std::cout << "Insertion sort elapsed CPU time:";
-    }
-    BOOST_CHECK(std::is_sorted(begin(testData), end(testData)));
-
-    std::shuffle(begin(testData), end(testData), rng);
-
-    {
-        boost::timer::auto_cpu_timer t(3);
-        quickSort(begin(testData), end(testData));
-        std::cout << "Quick sort elapsed CPU time:";
-    }
-    BOOST_CHECK(std::is_sorted(begin(testData), end(testData)));
-
+BOOST_AUTO_TEST_CASE(EmptyQuicksort)
+{
     std::vector<int> emptyContainer;
-    quickSort(begin(emptyContainer), end(emptyContainer));
+    BOOST_CHECK_NO_THROW(quickSort(begin(emptyContainer), end(emptyContainer)));
 }
